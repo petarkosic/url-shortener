d2c54ba83d4fba05fb4fbf033c25981ef053bb42
petarkosic <avt3914@gmail.com>
2023-03-01 23:59:33 +0100

    ADD - history

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,9 +5,11 @@ import './App.css';
 function App() {
 	const [longUrl, setLongUrl] = useState<string>('');
 	const [data, setData] = useState<any>(null);
+	const [history, setHistory] = useState<string[]>([]);
 
 	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
+		setHistory((prevHistory) => [...prevHistory, longUrl]);
 		if (longUrl) {
 			const fetchData = await fetch('http://localhost:5000/api', {
 				method: 'POST',
@@ -24,21 +26,35 @@ function App() {
 
 	return (
 		<div className='App'>
-			<div className='form__wrapper'>
-				<form onSubmit={handleSubmit}>
-					<input
-						type='text'
-						value={longUrl}
-						onChange={(e) => setLongUrl(e.target.value)}
-						placeholder='Enter URL'
-					/>
-					<button type='submit'>Submit</button>
-				</form>
-			</div>
-			<div className='data__value'>
-				<Link to={data?.hashValue} target='_blank'>
-					{data?.hashValue}
-				</Link>
+			{history.length > 0 && (
+				<div className='history'>
+					<p>History:</p>
+					{history.map((url, index) => (
+						<li key={index}>
+							<Link to={url} target='_blank'>
+								{url}
+							</Link>
+						</li>
+					))}
+				</div>
+			)}
+			<div className='form__container'>
+				<div className='form__wrapper'>
+					<form onSubmit={handleSubmit}>
+						<input
+							type='text'
+							value={longUrl}
+							onChange={(e) => setLongUrl(e.target.value)}
+							placeholder='Enter URL'
+						/>
+						<button type='submit'>Submit</button>
+					</form>
+				</div>
+				<div className='data__value'>
+					<Link to={data?.hashValue} target='_blank'>
+						{data?.hashValue}
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
